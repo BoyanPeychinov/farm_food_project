13303f1eb52f02460138194441457e0c8e7d14f2
BoyanPeychinov <BoyanPeychinov@gmail.com>
2021-08-03 17:26:30 +0300

    Added extended User class

--- farm_food_project/farm_food_auth/models.py
+++ farm_food_project/farm_food_auth/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+from farm_food_project.farm_food_auth.managers import FarmFoodUserManager
 
+
+class FarmFoodUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    USERNAME_FIELD = 'email'
+
+    objects = FarmFoodUserManager()
--- farm_food_project/settings.py
+++ farm_food_project/settings.py
@@ -147,3 +147,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'farm_food_auth.FarmFoodUser'
